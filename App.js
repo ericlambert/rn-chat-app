de7ff384e61c26a787fb46e079786fac0efd5fff
Eric <ericlambertone@gmail.com>
2021-06-22 19:07:48 -0700

    initial navigation in app

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
+
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
+const Stack = createStackNavigator();
+const globalScreenOptions = {
+  headerStyle: { backgroundColor : '#1e90ff' },
+  headerTitleStyle: { color: 'white' },
+  headerTintColor: { color: 'white' },
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Register' screenOptions={globalScreenOptions}>
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Register' component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
