e64e28c9ba5d6776306fda9ed2904104cfc5b92d
Eric <ericlambertone@gmail.com>
2021-06-29 20:13:57 -0700

    ChatScreen header customized

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,7 +1,45 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { Avatar, } from 'react-native-elements'
+import {AntDesign, FontAwesome, Ionicons} from '@expo/vector-icons'
 
-const ChatScreen = () => {
+const ChatScreen = ( {navigation, route} ) => {
+    const { chatName }  = route.params
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerBackTitleVisible: false,
+            headerTitleAlign: 'left',
+            headerTitle: () => (
+                <View style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                }}>
+                    <Avatar
+                        rounded
+                        source={{uri: 'https://www.bigleaf.net/wp-content/uploads/2017/10/avatar-placeholder.png'}} />
+                    <Text style={{ color: 'white', marginLeft: 10, fontWeight: '700'}}>{chatName}</Text>
+                </View>
+            ),
+
+            headerLeft: () => (
+                <TouchableOpacity style={{marginLeft: 10}} onPress={navigation.goBack}>
+                    <AntDesign name='arrowleft' size={24} color='white'/>
+                </TouchableOpacity>
+            ),
+
+            headerRight: () => (
+                <View style={{flexDirection: 'row', width: 60, justifyContent: 'space-between', marginRight: 10}}>
+                    <TouchableOpacity>
+                        <FontAwesome name='video-camera' size={24} color='white' />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name='call' size={24} color='white' />
+                    </TouchableOpacity>
+                </View>
+            )
+        })
+    }, [navigation])
     return (
         <View>
             <Text>Chat Messages</Text>
